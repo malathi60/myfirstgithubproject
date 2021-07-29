960d5dc57c8206277f9e436c917397f08a241db1
malathi <poloju269@gmail.com>
2021-07-29 14:06:32 +0530

    my second commit

--- Selenium/org.github.project/src/test/java/mypack/Calculation.java
+++ Selenium/org.github.project/src/test/java/mypack/Calculation.java
@@ -1,8 +1,12 @@
 package mypack;
 
+import org.testng.annotations.Test;
+
 public class Calculation {
 
-	public static void main(String[] args) {
+	@Test
+	public void addition() 
+	{
 		int x=100;
 		int y=200;
 		int z=x+y;
