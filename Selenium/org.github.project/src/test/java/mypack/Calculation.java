8d9ec87552dba4a20371f618015c8f06863b67ce
malathi60 <88128247+malathi60@users.noreply.github.com>
2021-07-29 12:27:29 +0530

    Update Calculation.java

--- Selenium/org.github.project/src/test/java/mypack/Calculation.java
+++ Selenium/org.github.project/src/test/java/mypack/Calculation.java
@@ -3,8 +3,8 @@ package mypack;
 public class Calculation {
 
 	public static void main(String[] args) {
-		int x=10;
-		int y=20;
+		int x=100;
+		int y=200;
 		int z=x+y;
 		System.out.println("output is :" + z);
 
