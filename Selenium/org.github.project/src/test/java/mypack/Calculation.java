6ab81a121c7a53179460f1d45b832307e6348438
malathi <poloju269@gmail.com>
2021-07-29 12:21:04 +0530

    my first commit

---
+++ Selenium/org.github.project/src/test/java/mypack/Calculation.java
@@ -0,0 +1,13 @@
+package mypack;
+
+public class Calculation {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=20;
+		int z=x+y;
+		System.out.println("output is :" + z);
+
+	}
+
+}
